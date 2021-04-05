8c241f0139937bba00d54f2a3bb9d183eb4b182f
Yasir323 <yasir.jafri007@gmail.com>
2021-04-05 20:47:56 +0530

    Merge Sort

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -12,8 +12,20 @@ def search(array, size, x):
 
 def main():
 	array = np.random.randint(low=1, high=1000000, size=9999999)
+	li = list(array)
 	query = 786
 	size = len(array)
+
+	start_time = time.time()
+	result = search(li, size, query)
+	stop_time = time.time()
+	if result == -1:
+		rprint("[red]Element is not present in the array.")
+	else:
+		rprint("[green]ELement found in the array at index:",result)
+	rprint("[yellow]Total time taken to search a list:", stop_time - start_time, "[yellow]seconds.")
+
+
 	start_time = time.time()
 	result = search(array, size, query)
 	stop_time = time.time()
@@ -21,8 +33,7 @@ def main():
 		rprint("[red]Element is not present in the array.")
 	else:
 		rprint("[green]ELement found in the array at index:",result)
-	rprint("[yellow]Total time taken:", stop_time - start_time, "[yellow]seconds.")
-
+	rprint("[yellow]Total time taken to search a numpy array:", stop_time - start_time, "[yellow]seconds.")
 
 if __name__ == "__main__":
 	main()
--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -13,31 +13,62 @@ import time
 from rich import print as rprint 
 
 
-def selection_sort(array, size):
-	for i in range(1, size):
-		j = i
-		while j > 0:
-			if array[j] < array[j - 1]:
-				array[j], array[j - 1]	= array[j - 1], array[j]
-			j -= 1
 
+def merge_sort(array):
+    if len(array) > 1:
+ 
+         # Finding the mid of the array
+        mid = len(array)//2
+ 
+        # Dividing the array elements
+        L = array[:mid]
+ 
+        # into 2 halves
+        R = array[mid:]
+ 
+        # Sorting the first half
+        merge_sort(L)
+ 
+        # Sorting the second half
+        merge_sort(R)
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrayays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            array[k] = R[j]
+            j += 1
+            k += 1
 
 def main():
-	array = np.random.randint(low=1, high=50, size=10)
-	size = len(array)
-	big_array = np.random.randint(low=1, high=100000, size=5000)
-	big_size = len(big_array)
-	rprint("[green]Unsorted array:",array)
-	selection_sort(array, size)
-	rprint("[green]Sorted array:", array)
-	times = np.array([])
-	for i in range(10):
-		start_time = time.time()
-		selection_sort(big_array, big_size)
-		stop_time = time.time()
-		times = np.append(times, [stop_time - start_time])
-	rprint("[yellow]Average Time taken over 10 sorting operations to sort an array of size 5000:", times.mean(), "[yellow]seconds.")
+        array = list(np.random.randint(low=1, high=50, size=10))
+        big_array = list(np.random.randint(low=1, high=100000, size=5000))
+        rprint("[green]Unsorted array:",array)
+        merge_sort(array)
+        rprint("[green]Sorted array:", array)
+        times = np.array([])
+        for i in range(10):
+              start_time = time.time()
+              merge_sort(big_array)
+              stop_time = time.time()
+              times = np.append(times, [stop_time - start_time])
+        rprint("[yellow]Average Time taken over 10 sorting operations to sort an array of size 5000:", times.mean(), "[yellow]seconds.")
 
 
 if __name__ == "__main__":
-	main()
+        main()
